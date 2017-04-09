4588798567dc8948bafedb3429ad7c3bb4ce6b1a
mathieu <mathieu.c.sabourin@gmail.com>
2017-04-08 19:19:43 -0700

    Implement recursive diff.

--- src/spellbound/lib/diff.py
+++ src/spellbound/lib/diff.py
@@ -1,4 +1,6 @@
-def diff(a, b):
+from spellbound.lib.types import DictType
+
+def dict_diff(a, b):
     a_keys, b_keys, = a.types.keys(), b.types.keys()
     additions = [
         '<{}: {}>'.format(k, b.types[k])
@@ -8,16 +10,23 @@ def diff(a, b):
         '<{}: {}>'.format(k, a.types[k])
         for k in a_keys - b_keys
     ]
-    changes = [
-        '<{}: {} != {}>'.format(
-            k, a.types[k], b.types[k]
-        )
+    changes = {
+        k: diff(a.types[k], b.types[k])
         for k in a_keys & b_keys
         if a.types[k] != b.types[k]
-    ]
+    }
 
     return {
         '+': additions,
         '-': deletions,
         '~': changes
     }
+
+def diff(a, b):
+    if isinstance(a, DictType) and isinstance(b, DictType):
+        return dict_diff(a, b)
+    else:
+        return {
+            'expected': str(a),
+            'actual': str(b)
+        }
